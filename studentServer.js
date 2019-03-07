775a64b7cde889764a33504f08f5d961e24c581d
ruiiiiiur <jiangrui1123@outlook.com>
2019-03-07 12:44:34 -0500

    add code to process all the fields uploaded from the form

--- studentServer.js
+++ studentServer.js
@@ -110,6 +110,37 @@ app.post('/uploadData',function(req,res){
 			});
 
 
+app.post('/uploadData',function(req,res){
+			// note that we are using POST here as we are uploading data
+			// so the parameters form part of the BODY of the request rather than the RESTful API
+			console.dir(req.body);
+			pool.connect(function(err,client,done) {
+			if(err){
+			console.log("not able to get connection "+ err);
+			res.status(400).send(err);
+			}
+			var name = req.body.name;
+			var surname = req.body.surname;
+			var module = req.body.module;
+			var portnum = req.body.port_id;
+			var language = req.body.language;
+			var modulelist = req.body.modulelist;
+			var lecturetime = req.body.lecturetime;
+			var geometrystring = "st_geomfromtext('POINT("+req.body.longitude + " "+ req.body.latitude + ")')";
+			var querystring = "INSERT into formdata (name,surname,module, port_id,language,modulelist, lecturetime, geom) values ($1,$2,$3,$4,$5,$6,$7,";
+			var querystring = querystring + geometrystring + ")";
+			console.log(querystring);
+			client.query( querystring,[name,surname,module, portnum, language, modulelist, lecturetime],function(err,result) {
+			done();
+			if(err){
+			console.log(err);
+			res.status(400).send(err);
+			}
+			res.status(200).send("row inserted");
+			});
+			});
+			});
+
 // adding functionality to log the requests on the console as they come in 
 app.use(function (req, res, next) {
 			var filename = path.basename(req.url);
