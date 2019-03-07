8bf8ecde9b4a5f259cae81d2ca2984744742f9ab
ruiiiiiur <jiangrui1123@outlook.com>
2019-03-07 11:52:11 -0500

    body-parser;database connection;app.get

--- studentServer.js
+++ studentServer.js
@@ -1,3 +1,44 @@
+//be able to process the uploaded data
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+extended: true
+}));
+app.use(bodyParser.json());
+
+//Import the required database connectivity code and set up a database connection
+var fs = require('fs');
+var pg = require('pg');
+var configtext =
+""+fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
+// now convert the configruation file into the correct format -i.e. a name/value pair array
+var configarray = configtext.split(",");
+var config = {};
+for (var i = 0; i < configarray.length; i++) {
+var split = configarray[i].split(':');
+config[split[0].trim()] = split[1].trim();
+}
+var pool = new pg.Pool(config);
+
+//test out the connection
+app.get('/postgistest', function (req,res) {
+pool.connect(function(err,client,done) {
+if(err){
+console.log("not able to get connection "+ err);
+res.status(400).send(err);
+}
+client.query('SELECT name FROM london_poi' ,function(err,result) {
+done();
+if(err){
+console.log(err);
+res.status(400).send(err);
+}
+res.status(200).send(result.rows);
+});
+});
+});
+
+
+
 // express is the server that forms part of the nodejs program
 var express = require('express');
 var path = require("path");
