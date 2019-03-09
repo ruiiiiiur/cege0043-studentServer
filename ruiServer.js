ae297bb6af647771f00b8b3e5d8c7e0906a01290
ruiiiiiur <jiangrui1123@outlook.com>
2019-03-09 15:49:03 -0500

    checkpoint

--- ruiServer.js
+++ ruiServer.js
@@ -99,18 +99,6 @@ pool.connect(function(err,client,done) {
 
 
 
-// 2.4 actually do the POST request to studentServer.js 
-// and upload this to Ubuntu and GitHub
-// sent data to the server
-app.post('/reflectData',function(req,res){
-	// note that we are using POST here as we are uploading data
-	// so the parameters form part of the BODY of the request rather
-	//than the RESTful API
-	console.dir(req.body);
-	// for now, just echo the request back to the client
-	res.send(req.body);
-	});
-
 
 
 // 3.1 another get function
@@ -141,7 +129,95 @@ app.get('/getFormData/:port_id', function (req,res) {
 	});
 	});
 	});
-//geojson branch
+
+
+
+// 3.2 generate GeoJSON from any spatial table
+// specifying the tablename and the name of the geometry column
+app.get('/getGeoJSON/:tablename/:geomcolumn', function (req, res) {
+    pool.connect(function (err, client, done) {
+        if (err) {
+            console.log("not able to get connection " + err);
+            res.status(400).send(err);
+        }
+
+        var colnames = "";
+        var tablename = req.params.tablename;
+        var geomcolumn = req.params.geomcolumn;
+        console.log(req.params.tablename);
+        console.log(req.params.geomcolumn);
+        // first get a list of the columns that are in the table
+        // use string_agg to generate a comma separated list that can then be pasted into the next query
+        var querystring = "select string_agg(colname,',') from ( select column_name as colname ";
+        querystring += " FROM information_schema.columns as colname ";
+        querystring += " where table_name   = $1";
+        querystring += " and column_name <> $2";
+        querystring += " and data_type <> 'USER-DEFINED') as cols ";
+
+        console.log(querystring);
+
+        // now run the query
+        client.query(querystring, [tablename, geomcolumn], function (err, result) {
+            //call `done()` to release the client back to the pool
+            done();
+            if (err) {
+                console.log(err);
+                res.status(400).send(err);
+            }
+            thecolnames = result.rows[0]['string_agg'];
+            colnames = thecolnames;
+            console.log("the colnames " + thecolnames);
+
+            // now use the inbuilt geoJSON functionality
+            // and create the required geoJSON format using a query adapted from here:
+            // http://www.postgresonline.com/journal/archives/267-Creating-GeoJSON-Feature-Collections-with-JSON-and-PostGIS-functions.html, accessed 4th January 2018
+            // note that query needs to be a single string with no line breaks so built it up bit by bit
+
+            var querystring = " SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features  FROM ";
+            querystring += "(SELECT 'Feature' As type     , ST_AsGeoJSON(lg." + req.params.geomcolumn + ")::json As geometry, ";
+            querystring += "row_to_json((SELECT l FROM (SELECT " + colnames + ") As l      )) As properties";
+
+            // depending on whether we have a port number, do differen things
+            if (req.params.portNumber) {
+                querystring += "   FROM " + req.params.tablename + "  As lg where lg.port_id = '" + req.params.portNumber + "'  limit 100  ) As f ";
+            } else {
+                querystring += "   FROM " + req.params.tablename + "  As lg limit 100  ) As f ";
+            }
+            console.log(querystring);
+
+
+            // run the second query
+            client.query(querystring, function (err, result) {
+                //call `done()` to release the client back to the pool
+                done();
+                if (err) {
+                    console.log(err);
+                    res.status(400).send(err);
+                }
+                res.status(200).send(result.rows);
+            });
+
+        });
+    });
+});
+
+
+
+
+
+// 2.4 actually do the POST request to studentServer.js 
+// and upload this to Ubuntu and GitHub
+// sent data to the server
+app.post('/reflectData',function(req,res){
+	// note that we are using POST here as we are uploading data
+	// so the parameters form part of the BODY of the request rather
+	//than the RESTful API
+	console.dir(req.body);
+	// for now, just echo the request back to the client
+	res.send(req.body);
+	});
+
+
 
 //2.5 & 2.6 connects to the database and inserts a record into the formData table
 app.post('/uploadData',function(req,res){
