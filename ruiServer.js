8c2d416d6e317c30a660d2f6fe7242d21bb0e595
ruiiiiiur <jiangrui1123@outlook.com>
2019-03-09 10:00:15 -0500

    serve static fils

--- ruiServer.js
+++ ruiServer.js
@@ -30,20 +30,25 @@ app.get('/',function (req,res) {
 
 //5. the server returns test.html when it is requested
 // serve an HTML file
-app.get('/test.html', function (req, res) {
+//app.get('/test.html', function (req, res) {
 	// run some server-side code
-	console.log('test.html requested');
+//	console.log('test.html requested');
 	// note that __dirname gives the path to the studentServer.js file
-	res.sendFile(__dirname + '/test.html');
-	});
+//	res.sendFile(__dirname + '/test.html');
+//	});
 
 
-// serve any HTML file
-app.get('/:fileName', function (req, res) {
+// 6. serve any HTML file
+//app.get('/:fileName', function (req, res) {
 	// run some server-side code
-	var fileName = req.params.fileName;
-	console.log(fileName + ' requested');
+//	var fileName = req.params.fileName;
+//	console.log(fileName + ' requested');
 	// note that __dirname gives the path to the studentServer.js file
-	res.sendFile(__dirname + '/'+ fileName);
-	});
+//	res.sendFile(__dirname + '/'+ fileName);
+//	});
 
+
+
+// 7. serve static files - e.g. html, css
+// this should always be the last line in the server file
+app.use(express.static(__dirname));
