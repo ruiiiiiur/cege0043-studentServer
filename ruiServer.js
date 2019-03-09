72bf124cdc5e40568c892ba1fb2264361d5214fc
ruiiiiiur <jiangrui1123@outlook.com>
2019-03-09 10:19:43 -0500

    be able to process the uploaded data

--- ruiServer.js
+++ ruiServer.js
@@ -1,20 +1,27 @@
 
-// 1. express is the server that forms part of the nodejs program
+// 1.1. express is the server that forms part of the nodejs program
 var express = require('express');
 var path = require("path");
 var app = express();
 
 
 
-// 2. add an http server to serve files to the Edge browser
+// 1.2. add an http server to serve files to the Edge browser
 // due to certificate issues it rejects the https files if they are not
 // directly called in a typed URL
 var http = require('http');
 var httpServer = http.createServer(app);
 httpServer.listen(4480);
 
+// 2.1 process the uploaded data
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+	extended: true
+	}));
+app.use(bodyParser.json());
 
-// 8. “cross origin request”
+
+// 1.8. “cross origin request”
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -22,7 +29,7 @@ app.use(function(req, res, next) {
 	});
 
 
-// 4. adding functionality to log the requests
+// 1.4. adding functionality to log the requests
 app.use(function (req, res, next) {
 	var filename = path.basename(req.url);
 	var extension = path.extname(filename);
@@ -30,13 +37,13 @@ app.use(function (req, res, next) {
 	next();
 	});
 
-// 3. 
+// 1.3. 
 app.get('/',function (req,res) {
 	res.send("hello world from the HTTP server (Rui Server)");
 	});
 
 
-//5. the server returns test.html when it is requested
+//1.5. the server returns test.html when it is requested
 // serve an HTML file
 //app.get('/test.html', function (req, res) {
 	// run some server-side code
@@ -46,7 +53,7 @@ app.get('/',function (req,res) {
 //	});
 
 
-// 6. serve any HTML file
+// 1.6. serve any HTML file
 //app.get('/:fileName', function (req, res) {
 	// run some server-side code
 //	var fileName = req.params.fileName;
@@ -57,6 +64,6 @@ app.get('/',function (req,res) {
 
 
 
-// 7. serve static files - e.g. html, css
+// 1.7. serve static files - e.g. html, css
 // this should always be the last line in the server file
 app.use(express.static(__dirname));
