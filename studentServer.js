dc0e46bc9a3571f5bb0a336ab1af08f651773683
ruiiiiiur <jiangrui1123@outlook.com>
2019-03-09 07:54:58 -0500

    delete the repeated code

--- studentServer.js
+++ studentServer.js
@@ -2,6 +2,33 @@
 var express = require('express');
 var path = require("path");
 var app = express();
+
+
+
+//Import the required database connectivity code and set up a database connection
+var fs = require('fs');
+var pg = require('pg');
+var configtext = "" + fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
+
+// now convert the configruation file into the correct format -i.e. a name/value pair array
+var configarray = configtext.split(",");
+var config = {};
+for (var i = 0; i < configarray.length; i++) {
+     var split = configarray[i].split(':');
+     config[split[0].trim()] = split[1].trim();
+}
+
+var pool = new pg.Pool(config);
+
+
+//be able to process the uploaded data
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+extended: true
+}));
+app.use(bodyParser.json());
+
+
 // add an http server to serve files to the Edge browser
 // due to certificate issues it rejects the https files if they are not
 // directly called in a typed URL
@@ -11,12 +38,7 @@ httpServer.listen(4480);
 
 
 
-//be able to process the uploaded data
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({
-extended: true
-}));
-app.use(bodyParser.json());
+
 
 
 
@@ -31,24 +53,11 @@ app.use(function(req, res, next) {
 
 
 
-//Import the required database connectivity code and set up a database connection
-var fs = require('fs');
-var pg = require('pg');
-var configtext =
-""+fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
-// now convert the configruation file into the correct format -i.e. a name/value pair array
-var configarray = configtext.split(",");
-var config = {};
-for (var i = 0; i < configarray.length; i++) {
-var split = configarray[i].split(':');
-config[split[0].trim()] = split[1].trim();
-}
-var pool = new pg.Pool(config);
-
 //You should see ‘hello world from the HTTP server’
 app.get('/',function (req,res) {
 			res.send("hello world from the HTTP server(studentserver)");
 			});
+
 
 
 //test out the connection
@@ -58,6 +67,7 @@ app.get('/postgistest', function (req,res) {
 			console.log("not able to get connection "+ err);
 			res.status(400).send(err);
 			}
+
 			client.query('SELECT name FROM london_poi' ,function(err,result) {
 			done();
 			if(err){
@@ -82,36 +92,6 @@ app.post('/reflectData',function(req,res){
 			console.dir(req.body);
 			// for now, just echo the request back to the client
 			res.send(req.body);
-			});
-
-
-app.post('/uploadData',function(req,res){
-			// note that we are using POST here as we are uploading data
-			// so the parameters form part of the BODY of the request rather than the
-			// RESTful API
-			console.dir(req.body);
-			pool.connect(function(err,client,done) {
-			if(err){
-			console.log("not able to get connection "+ err);
-			res.status(400).send(err);
-			}
-
-			var name = req.body.name;
-			var surname = req.body.surname;
-			var module = req.body.module;
-			var portnum = req.body.port_id;
-
-			var querystring = "INSERT into formdata (name,surname,module, port_id) values ($1,$2,$3,$4) ";
-			console.log(querystring);
-			client.query( querystring,[name,surname,module,portnum],function(err,result) {
-			done();
-			if(err){
-			console.log(err);
-			res.status(400).send(err);
-			}
-			res.status(200).send("row inserted");
-			});
-			});
 			});
 
 
